73d202fe50cccaf5a6b30e1e2e51cee0c19e30c4
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-06 19:15:06 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const changeColorButton = document.querySelector('.change-color');
+  const colorSpan = document.querySelector('.color');
+
+  changeColorButton.addEventListener('click', function () {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
+});
