28cedc384732e820d7261a3c405220ce67dee156
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-06 14:14:38 +0100

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,16 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+const galleryHTML = images
+  .map(
+    (image) => `
+      <li>
+        <img src="${image.url}" alt="${image.alt}">
+      </li>
+    `
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryHTML);
