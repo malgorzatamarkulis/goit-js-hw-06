5241a6b5f954842361bbb2690735f6552141205f
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-06 19:37:02 +0100

    task 6 correct

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inputElement = document.getElementById('validation-input') as HTMLInputElement | null;
-
-    if (inputElement) {
-        inputElement.addEventListener('blur', () => {
-            const inputValue = inputElement.value;
-            const dataLengthAttribute = inputElement.getAttribute('data-length');
-
-            if (dataLengthAttribute !== null) {
-                const expectedLength = parseInt(dataLengthAttribute, 10);
-
-                if (inputValue.length === expectedLength) {
-                    inputElement.classList.remove('invalid');
-                    inputElement.classList.add('valid');
-                } else {
-                    inputElement.classList.remove('valid');
-                    inputElement.classList.add('invalid');
-                }
-            }
-        });
-    }
+const input = document.getElementById("validation-input"); input.addEventListener("blur", () => {
+    const value = input.value;
+    const dataLength = parseInt(input.getAttribute("data-length"));
+    if (value.length === dataLength) { input.classList.remove("invalid"); input.classList.add("valid"); }
+    else { input.classList.remove("valid"); input.classList.add("invalid"); }
 });
