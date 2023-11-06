7faa8a4d0d3d86bba245177604fea358f267199e
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-06 19:13:03 +0100

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.querySelector('.login-form');
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const emailInput = loginForm.elements.email;
+        const passwordInput = loginForm.elements.password;
+
+        if (!emailInput.value || !passwordInput.value) {
+            alert('Wszystkie pola powinny zostać wypełnione.');
+        } else {
+            const formData = {
+                email: emailInput.value,
+                password: passwordInput.value,
+            };
+
+            console.log(formData);
+            loginForm.reset();
+        }
+    });
+});
