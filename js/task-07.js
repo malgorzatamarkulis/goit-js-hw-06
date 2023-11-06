1d35386d2a923b8afd0940ab90033b07035f2181
MalgorzataM <malgorzata.markulis@gmail.com>
2023-11-06 19:08:22 +0100

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fontSizeControl = document.getElementById('font-size-control');
+    const textElement = document.getElementById('text');
+
+    fontSizeControl.addEventListener('input', function () {
+        const fontSize = fontSizeControl.value + 'px';
+        textElement.style.fontSize = fontSize;
+    });
+});
